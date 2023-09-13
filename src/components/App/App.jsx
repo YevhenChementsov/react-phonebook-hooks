5399937ab4141c9ca54aa8f2d1b15a5fb38016dd
Yevhen Chementsov <yevhenchementsov@gmail.com>
2023-09-13 16:16:09 +0300

    Update useState logical operator in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -21,7 +21,7 @@ import {
 const App = () => {
   const [contacts, setContacts] = useState(
     () =>
-      JSON.parse(window.localStorage.getItem('contactList')) || [
+      JSON.parse(window.localStorage.getItem('contactList')) ?? [
         { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
         { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
         { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
